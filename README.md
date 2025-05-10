10c277b2513936f2db71289b057324b9e4b7524d
Piyush <piyushgarg80>
2025-05-10 18:59:51 +0530

    updated plugin

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # mpv-http-mitmytproxy
+
+The script will automatically setup the mpv to inject mitmproxy.
+
+
+## Installation / Setup
+
+```
+git clone git@github.com:piyushgarg/mpv-http-mitmytproxy.git
+cd mpv-http-mitmytproxy
+mkdir -p ~/.config/mpv/scripts/proxy
+cp -fv mitmplugin.py main.lua ~/.config/mpv/scripts/proxy
+```
+
+### Update the path for mitmdump
+Edit and update the actual path for mitmdump in ~/.config/mpv/scripts/proxy/main.lua
+
+
+### credits / source
+[mpv-http-ytproxy](https://github.com/spvkgn/mpv-http-ytproxy)
+
+[gist](https://gist.github.com/ftk/253347b2c9a53bbd6087f086970106b6)
+
+[toomyzoom](https://github.com/toomyzoom)
--- main.lua
+++ main.lua
@@ -17,26 +17,25 @@ local function init()
         return
     end
 
+    print("starting mitmproxy...")
     -- launch mitm proxy
     local args = {
-        mp.get_script_directory() .. "/http-ytproxy",
-        "-c", mp.get_script_directory() .. "/cert.pem",
-        "-k", mp.get_script_directory() .. "/key.pem",
-        "-r", "10485760", -- range modification
-        "-p", "12081" -- proxy port
+        os.getenv("HOME") .. "/venv/bin/mitmdump",
+        "-s", mp.get_script_directory() .. "/mitmplugin.py",
+        "--set", "web_open_browser=false",
+        "--mode", "regular@12081", -- proxy port
     }
     mp.command_native_async({
-        name = "subprocess",
-        capture_stdout = false,
-        playback_only = false,
-        args = args,
+            name = "subprocess",
+            capture_stdout = true,
+            capture_stderr = true,
+            playback_only = true,
+            args = args,
     });
 
     mp.set_property("http-proxy", "http://127.0.0.1:12081")
     mp.set_property("tls-verify", "no")
     -- this is not really needed
-    --mp.set_property("tls-verify", "yes")
-    --mp.set_property("tls-ca-file", mp.get_script_directory() .. "/cert.pem")
 end
 
 mp.register_event("start-file", init)
